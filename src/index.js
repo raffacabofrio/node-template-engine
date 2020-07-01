74f649779aaa97b82810714ed076cde6a54dfe0c
Raffaello Damgaard <raffacabofrio@gmail.com>
2020-07-01 04:14:49 -0300

    Add template engine.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { addHook } = require('pirates')
+const fs = require('fs')
+
+const matcher = (filename) => true
+const hook = (code, filename) => `
+  const fs = require('fs')
+  const templateEngine = ${templateEngine.toString()}
+  module.exports = (data) => templateEngine(data, '${filename}')
+`
+
+const templateEngine = (data, templatePath) => {
+  var template = fs.readFileSync(templatePath).toString()
+
+  if (typeof data != 'object') throw new Error('Invalid data for template. Must be object.')
+
+  for (var propertyName in data) {
+    var type = typeof data[propertyName]
+    if (type != 'string' && type != 'number')
+      throw new Error(`Invalid data on attibute ${propertyName}. Must be string or number.`)
+
+    var re = new RegExp(`{{${propertyName}}}`, 'g')
+    template = template.replace(re, data[propertyName])
+  }
+
+  return template
+}
+
+addHook(hook, { exts: ['.chop'], matcher })
